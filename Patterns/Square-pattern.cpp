3fc274018957bce216696af3ac1d2f6dc4802ea5
AnkushSaral <ankusaral74@gmail.com>
2026-05-20 00:21:48 +0530

    Add 1 more square pattern and 2 triangle patterns

--- Patterns/Square-pattern.cpp
+++ Patterns/Square-pattern.cpp
@@ -1,31 +1,69 @@
-
-// Square Pattern
-
-// Input: n = 4
-
-// Output:
-// 4 4 4 4
-// 4 4 4 4
-// 4 4 4 4
-// 4 4 4 4
-
-
 #include <iostream>
 using namespace std;
 
 
 
 int main(){
-    int n;
-    cout << "Enter the size of the square pattern: ";
-    cin >> n;
 
-    for (int i = 0; i < n; i++){
+
+// Square Pattern
+
+//Type 1
+
+// Input: n = 4
+// Output: // 4 4 4 4
+           // 4 4 4 4
+           // 4 4 4 4
+           // 4 4 4 4
+
+// Code for type 1
+    // int n;
+    // cout << "Enter the size of the square pattern: ";
+    // cin >> n;
+
+    // for (int i = 0; i < n; i++){
     
-        for (int j = 0; j < n; j++){
-        cout << n << " ";
-    }
-        cout << endl;
+    //     for (int j = 0; j < n; j++){
+    //     cout << n << " ";
+    // }
+    //     cout << endl;
 
-    }
+    // }
+
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+//Type 2
+
+// Input: n = 3
+// Output: // A B C
+           // D E F
+           // G H I
+           
+    //Code for type 2
+    // int n;
+    // cout << "Enter the size of the square pattern: ";
+    // cin >> n;
+
+    // char character = 'A';
+    // for (int i = 0; i < n; i++){
+    
+    //     for (int j = 0; j < n; j++){
+    //     cout << character << " ";
+    //     character++;
+    // }
+    //     cout << endl;
+
+    // }
+
+
+
+
 }
---
+++ Patterns/Triangle-pattern.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+
+
+// Triangle Pattern
+
+//Type 1
+
+// Input: n = 4
+// Output: // *
+           // * *
+           // * * *
+           // * * * *
+
+    // Code for type 1
+    // int n;
+    // cout << "Enter the size of the triangle pattern: ";
+    // cin >> n;
+
+    // for (int i = 0; i < n; i++){
+    
+    //     for (int j = 0; j <= i; j++){
+    //     cout << "*" << " ";
+    // }
+    //     cout << endl;
+
+    // }
+
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+//Type 2
+
+// Input: n = 4
+// Output: // 1
+           // 22
+           // 333
+           // 4444
+
+    //Code for type 2
+    // int n;
+    // cout << "Enter the size of the triangle pattern: ";
+    // cin >> n;
+
+    // for (int i = 0; i < n; i++){
+    
+    //     for (int j = 0; j <= i; j++){
+    //     cout << (i + 1) << " ";
+
+    // }
+    //     cout << endl;
+
+    // }
+
+
+
+
+}
